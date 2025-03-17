37fe65c296c64843bbd1a80dc7d1ab91aeab4d3d
Aljel <makakwar@gmail.com>
2025-03-17 21:27:23 +0400

    Refactor fib function un main.cpp

--- main.cpp
+++ main.cpp
@@ -3,15 +3,18 @@
 int fib(int n){
     if (n <= 0)
         return 0;
-    else if(n == 1 || n == 2)
-        return 1;
-    else 
-        return fib(n-1) + fib(n-2);
+    int a = 0, b = 1;
+    for (int i = 0; i < n; i++){
+        std::cout << a << "\n";
+        int next = a + b;
+        a = b;
+        b = next;
+    }
 }
 int main(){
     std::cout <<"Hello, World! Enter an integer number\n";
     int n;
     std::cin >> n;
-    std::cout << fib(n-1);
+    fib(n);
     return 0;
 }
