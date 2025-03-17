8053d9cd92082f5d2713d6d02ed53217a8d7a6fb
Aljel <makakwar@gmail.com>
2025-03-17 21:13:10 +0400

    Added fib function to main.cpp file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 
+int fib(int n){
+    if (n <= 0)
+        return 0;
+    else if(n == 1 || n == 2)
+        return 1;
+    else 
+        return fib(n-1) + fib(n-2);
+}
 int main(){
-    std::cout <<"Hello, World!";
+    std::cout <<"Hello, World! Enter an integer number\n";
+    int n;
+    std::cin >> n;
+    std::cout << fib(n);
     return 0;
 }
